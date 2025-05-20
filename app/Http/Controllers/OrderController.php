0d087a2ecccce88ac6d48f00095e75c58076c9b5
Ahmad Saifudin Syuhri <ahmadsaepudin032@gmail.com>
2025-05-21 01:27:30 +0700

    add order model, controller, and route

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,7 +11,7 @@ class OrderController extends Controller
         $order = new Order();
         $order->id_account = $request->id_account;
         $order->nickname = $request->nickname;
-        $order->paymentMethod = $request->paymentMethod;
+        $order->payment_method = $request->payment_method;
         $order->pay_price = $request->pay_price;
         $order->pay_tax = $request->pay_tax;
         $order->pay_total = $request->pay_total;
